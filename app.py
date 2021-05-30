b7a1f6eb204dcb932fe67869d8a1a5b3bb07b5d1
Jakub Filinger <kubafilinger@gmail.com>
2021-05-30 11:30:01 +0200

    Add static files and base app structure

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-	return "xDDDDDDDDDDDD"
+def index():
+	return render_template('index.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html', name="Kuba", lastName="Filinger")
+
+@app.route('/gallery')
+def gallery():
+	return render_template('gallery.html')
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
